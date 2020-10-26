2b3feb8134f0ff0aa40b9c1c02030dc3ac1d938e
QuangNam <quangnamhd9x@gmail.com>
2020-10-26 14:54:36 +0700

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { RegisterComponent } from './register/register.component';
-import { RegisterFinalComponent } from './register-final/register-final.component';
-import { LoginComponent } from './login/login.component';
-import { TodoComponent } from './todo/todo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenFormComponent,
     RegisterComponent,
-    RegisterFinalComponent,
-    LoginComponent,
-    TodoComponent
   ],
   imports: [
     BrowserModule,
